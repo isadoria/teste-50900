130a1e931be87a88d2868a3309635d12e82c69c5
Isadora Dória <96890864+isadoria@users.noreply.github.com>
2025-11-28 15:14:48 -0300

    Add simple greeting application in app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# Aplicação Python simples
+def saudacao(nome):
+    return f"Olá, {nome}! Bem-vindo ao projeto seguro!"
+
+if __name__ == "__main__":
+    print(saudacao("DevSecOps"))
+    print("✅ Aplicação executada com sucesso!")
+
